34a8e6eb3a9316b64f8b9271f0ff6b6d3198970a
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-05 14:16:51 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [tabNum, setTabNum] = useState(["Tab 1", "Tab 2", "Tab 3"]);
+  const [tabNumSelected, setTabNumSelected] = useState("Tab 1");
+  const [tabAlph, setTabAlph] = useState(["Tab A", "Tab B", "Tab C"]);
+  const [tabAlphSelected, setTabAlphSelected] = useState("Tab A");
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+        {tabNum.map(tab => 
+          <li key={tab} onClick={() => setTabNumSelected(tab)}>
+            {tab}
+          </li>
+        )}
+      </ul>
+      <p>Content for {tabNumSelected}</p>
+      <br />
+      <ul>
+        {tabAlph.map(tab => 
+          <li key={tab} onClick={() => setTabAlphSelected(tab)}>
+            {tab}
+          </li>
+        )}
+      </ul>
+      <p>Content for {tabAlphSelected}</p>
     </div>
   )
 }
